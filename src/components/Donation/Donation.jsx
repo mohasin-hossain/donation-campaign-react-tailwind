99e5dced6f50206bb8615223a952bba7130a3cbe
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-03 11:04:34 +0600

    Donations fetch, Dynamic colors

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -1,10 +1,41 @@
+const Donation = ({ donation }) => {
+  const {
+    id,
+    donation_title,
+    img,
+    donation_category,
+    color_primary,
+    color_secondary,
+    color_tertiary,
+  } = donation;
 
-const Donation = () => {
-    return (
-        <div>
-            <h1>Donation</h1>
-        </div>
-    );
+  return (
+    <div
+      className="rounded-md"
+      style={{
+        "--color-primary": color_primary,
+        "--color-secondary": color_secondary,
+        "--color-tertiary": color_tertiary,
+        backgroundColor: "var(--color-tertiary)",
+      }}
+    >
+      <img src={img} alt="" />
+      <div className=" p-4 flex flex-col">
+        <span
+          className="px-2 py-1 rounded-md self-start"
+          style={{
+            color: "var(--color-primary)",
+            backgroundColor: "var(--color-secondary)",
+          }}
+        >
+          {donation_category}
+        </span>
+        <h3 className="mt-2 text-xl" style={{ color: "var(--color-primary)" }}>
+          {donation_title}
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Donation;
--- src/components/DonationList/DonationList.jsx
+++ src/components/DonationList/DonationList.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
+import Donation from "../Donation/Donation";
 
 const DonationList = () => {
-    return (
-        <div>
-            <h1>DonationList</h1>
-        </div>
-    );
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    fetch("donation.json")
+      .then((res) => res.json())
+      .then((data) => setDonations(data));
+  }, []);
+
+  return (
+    <div className="container mx-auto">
+      <h1>DonationList {donations.length}</h1>
+      <div className="grid grid-cols-4 gap-8">
+        {donations.map((donation, idx) => (
+          <Donation key={idx} donation={donation}></Donation>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DonationList;
