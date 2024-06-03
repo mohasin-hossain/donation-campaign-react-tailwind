c90a19a4d1c9a7cf83dc2e24e507e3cdc17a911e
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-03 14:02:04 +0600

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
-
 const Footer = () => {
-    return (
-        <div>
-            <h1>Footer</h1>
-        </div>
-    );
+  return (
+    <div>
+      <footer className="footer footer-center p-4 text-base-content bg-gray-100 py-10 mt-20">
+        <aside>
+          <p className="text-base font-thin text-gray-600">In the realm of giving, surrounded by compassion, stands a guiding light—a social welfare foundation—dedicated to creating brighter futures.</p>
+        </aside>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
